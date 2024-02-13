7a02d3c601c3316cda105ba2444b27b256d0df2b
rizingblare <jd0419@pusan.ac.kr>
2024-02-13 15:35:47 +0900

    BetaChat Spring Boot Project Initialize
    - 스프릥 부트 프로젝트 생성 및 의존성 추가

---
+++ betachat/src/main/java/com/betamale/betachat/BetachatApplication.java
@@ -0,0 +1,13 @@
+package com.betamale.betachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetachatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetachatApplication.class, args);
+	}
+
+}
---
+++ betachat/src/test/java/com/betamale/betachat/BetachatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betamale.betachat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetachatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
