e1eaa4486e24cabfb43882724a99c2b561d21429
rizingblare <jd0419@pusan.ac.kr>
2024-02-13 15:39:51 +0900

    feat: User JPA Entity 생성
    - ID, Name, Email, Password를 갖는 회원 엔티티를 생성함

---
+++ betachat/src/main/java/com/betamale/betachat/user/User.java
@@ -0,0 +1,28 @@
+package com.betamale.betachat.user;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+}
