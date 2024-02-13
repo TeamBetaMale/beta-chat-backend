e418caebe9dde8d4b8347fa95bb7a32e45aa75ce
rizingblare <jd0419@pusan.ac.kr>
2024-02-13 16:37:44 +0900

    feat: signup 컨트롤러 메서드 구현
    - UserRestController에서 클라이언트의 signup 요청에 대한 응답 메서드
      구현

--- betachat/src/main/java/com/betamale/betachat/user/UserRestController.java
+++ betachat/src/main/java/com/betamale/betachat/user/UserRestController.java
@@ -1,6 +1,11 @@
 package com.betamale.betachat.user;
 
+import com.betamale.betachat._core.ApiUtils;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserRestController {
+    private final UserService userService;
 
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody @Valid UserRequest.SignUpDTO requestDTO) {
+        userService.addUser(requestDTO);
+        return ResponseEntity.ok().body(ApiUtils.success(null));
+    }
 }
